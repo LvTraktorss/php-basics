b628dfc1682885aa55e768cf6341f5cba9fb1a43
Emils <emils@gmail.com>
2025-10-30 11:39:15 +0200

    pievienoju function

---
+++ function.php
@@ -0,0 +1,22 @@
+<?php
+
+function intSequence($First, $Last){
+    for ($i=$First; $i <= $Last; $i++){
+        echo $i;
+        echo ":";
+    }
+}
+intSequence(5,8);
+intSequence(1,10);
+intSequence(7,16);
+
+
+
+
+
+
+
+
+
+
+?>
